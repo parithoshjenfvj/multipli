3608cf0ab8f3c7008a4fd1e2239f8680f5d9098c
parithoshjenfvj <143733121+parithoshjenfvj@users.noreply.github.com>
2024-04-22 18:43:38 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+public class Multiplication {
+    public static void main(String[] args) {
+        // Define the numbers to multiply
+        int num1 = 5;
+        int num2 = 10;
+
+        // Perform multiplication
+        int result = num1 * num2;
+
+        // Print the result
+        System.out.println("Result of multiplication: " + result);
+    }
+}
